6304e2bf04a56e2d0169194c1b27cbf862d3e7ba
Patrick Quinn <patrick.jm.quinn@gmail.com>
2025-10-21 22:54:16 +0100

    feat: add Ethernet detection and hardware availability to NetworkManager
    Detect WiFi and Bluetooth hardware presence via NetworkManager D-Bus
    
    Track Ethernet connections and connection names
    
    Emit wifiAvailable/bluetoothAvailable/ethernetConnected signals
    
    Query active connections for both WiFi and Ethernet simultaneously
    
    Handle systems without WiFi hardware gracefully
    
    All C++ backends now compile and integrate successfully

--- shell/src/networkmanagercpp.h
+++ shell/src/networkmanagercpp.h
@@ -15,6 +15,10 @@ class NetworkManagerCpp : public QObject
     Q_PROPERTY(bool wifiConnected READ wifiConnected NOTIFY wifiConnectedChanged)
     Q_PROPERTY(QString wifiSsid READ wifiSsid NOTIFY wifiSsidChanged)
     Q_PROPERTY(int wifiSignalStrength READ wifiSignalStrength NOTIFY wifiSignalStrengthChanged)
+    Q_PROPERTY(bool ethernetConnected READ ethernetConnected NOTIFY ethernetConnectedChanged)
+    Q_PROPERTY(QString ethernetConnectionName READ ethernetConnectionName NOTIFY ethernetConnectionNameChanged)
+    Q_PROPERTY(bool wifiAvailable READ wifiAvailable NOTIFY wifiAvailableChanged)
+    Q_PROPERTY(bool bluetoothAvailable READ bluetoothAvailable NOTIFY bluetoothAvailableChanged)
     Q_PROPERTY(bool bluetoothEnabled READ bluetoothEnabled NOTIFY bluetoothEnabledChanged)
     Q_PROPERTY(bool airplaneModeEnabled READ airplaneModeEnabled NOTIFY airplaneModeEnabledChanged)
 
@@ -26,6 +30,10 @@ public:
     bool wifiConnected() const { return m_wifiConnected; }
     QString wifiSsid() const { return m_wifiSsid; }
     int wifiSignalStrength() const { return m_wifiSignalStrength; }
+    bool ethernetConnected() const { return m_ethernetConnected; }
+    QString ethernetConnectionName() const { return m_ethernetConnectionName; }
+    bool wifiAvailable() const { return m_wifiAvailable; }
+    bool bluetoothAvailable() const { return m_bluetoothAvailable; }
     bool bluetoothEnabled() const { return m_bluetoothEnabled; }
     bool airplaneModeEnabled() const { return m_airplaneModeEnabled; }
 
@@ -47,24 +55,40 @@ signals:
     void wifiConnectedChanged();
     void wifiSsidChanged();
     void wifiSignalStrengthChanged();
+    void ethernetConnectedChanged();
+    void ethernetConnectionNameChanged();
+    void wifiAvailableChanged();
+    void bluetoothAvailableChanged();
     void bluetoothEnabledChanged();
     void airplaneModeEnabledChanged();
     void networkError(const QString& message);
 
+private slots:
+    void queryWifiState();
+    void queryConnectionState();
+    void updateWifiSignalStrength();
+    void updateWifiDetails();
+
 private:
     void setupDBusConnections();
-    void queryWifiState();
-    void updateWifiSignalStrength();
+    void detectHardwareAvailability();
 
     QDBusInterface* m_nmInterface;
     QTimer* m_signalMonitor;
+    QTimer* m_connectionMonitor;
     
     bool m_wifiEnabled;
     bool m_wifiConnected;
     QString m_wifiSsid;
     int m_wifiSignalStrength;
+    bool m_ethernetConnected;
+    QString m_ethernetConnectionName;
+    QString m_activeWifiDevicePath;
+    QString m_activeApPath;
     bool m_bluetoothEnabled;
     bool m_airplaneModeEnabled;
+    bool m_wifiAvailable;
+    bool m_bluetoothAvailable;
     bool m_hasNetworkManager;
 };
 
